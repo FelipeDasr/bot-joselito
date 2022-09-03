75f939fcdb9f5198305db909d2a8a0a9d8a7e87c
FelipeDasr <felipe.heiker07@gmail.com>
2022-09-03 11:45:47 -0400

    update

--- src/functions/help.ts
+++ src/functions/help.ts
@@ -6,7 +6,7 @@ export const help = async (msg: Message, client: Client) => {
     return await client.reply(
         chatId,
         "*👺 BOT JOSELITO 2.0*\n\n" +
-        "*>ajuda*. Exibir instruções de ajuda.\n" +
+        "*>ajuda*. Exibir instruções de ajuda.\n\n" +
         "*>figurinha*. Deve ser enviado com uma imagem ou um gif.\n\n" +
         "*O bot estará disponível das 8:40 até as 16:30, mas poderá estar indisponível as vezes.*\n\n" +
         "By: *Felipe Dos Anjos*",
