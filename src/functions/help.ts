285c50d2c0192ca65c19818e1707871cb19deaa2
FelipeDasr <felipe.heiker07@gmail.com>
2022-09-03 11:18:09 -0400

    new function (help)

---
+++ src/functions/help.ts
@@ -0,0 +1,15 @@
+import { Client, Message } from '@open-wa/wa-automate';
+
+export const help = async (msg: Message, client: Client) => {
+    const { id: msgId, chatId } = msg;
+
+    return await client.reply(
+        chatId,
+        "*👺 BOT JOSELITO 2.0*\n\n" +
+        "*>ajuda*. Exibir instruções de ajuda.\n" +
+        "*>figurinha*. Deve ser enviado com uma imagem ou um gif.\n\n" +
+        "*O bot estará disponível das 8:40 até as 16:30, mas poderá estar indisponível as vezes.*\n\n" +
+        "By: *Felipe Dos Anjos*",
+        msgId
+    );
+}
