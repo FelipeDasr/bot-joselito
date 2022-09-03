ad63f249f5107e0e09b19818e15fabd6d178bd4f
FelipeDasr <felipe.heiker07@gmail.com>
2022-09-03 19:37:13 -0400

    utils update

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,19 @@
+import { nanoid } from 'nanoid';
 import util from 'util';
 import fs from 'fs';
+import path from 'path';
 
 export const createFile = async (data: Buffer, fileFile: string) => {
-    const writeFile = util.promisify(fs.writeFileSync);
+    const mkdir = util.promisify(fs.mkdir);
+    const exists = util.promisify(fs.exists);
+    const writeFile = util.promisify(fs.writeFile);
+
+    const tempPath = path.resolve('src', 'temp');
+
+    if (!await exists(tempPath)) await mkdir(tempPath);
     await writeFile(fileFile, data, {});
+}
+
+export const generateId = () => {
+    return nanoid();
 }
