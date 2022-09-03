26e3bb1a93bcfe4648feb1bf9ea897864c951777
FelipeDasr <felipe.heiker07@gmail.com>
2022-09-03 11:25:19 -0400

    new attribute (client)

--- src/bot-handler/index.ts
+++ src/bot-handler/index.ts
@@ -1,29 +1,32 @@
-import { Client, Message, decryptMedia } from '@open-wa/wa-automate';
-import { help } from '../functions/help';
+import { Client, Message } from '@open-wa/wa-automate';
+
 import { makeSticker } from '../functions/makeSticker';
+import { help } from '../functions/help';
 
 export class BotHandler {
 
-    constructor() { }
+    constructor(
+        private client: Client
+    ) { }
 
-    private async messageTreatment(msg: Message, client: Client) {
+    private async messageTreatment(msg: Message) {
         if (!msg.isGroupMsg) return;
 
         switch (msg.text) {
             case '>ajuda':
-                return await help(msg, client);
+                return await help(msg, this.client);
 
             case '>figurinha':
-                return await makeSticker(msg, client);
+                return await makeSticker(msg, this.client);
 
             default:
                 return;
         }
     }
 
-    public async start(client: Client) {
-        await client.onAnyMessage(async msg => {
-            await this.messageTreatment(msg, client);
+    public async start() {
+        await this.client.onAnyMessage(async msg => {
+            await this.messageTreatment(msg);
         });
     }
 }
