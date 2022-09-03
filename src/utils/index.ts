5c937b407e56f3bdbb514278eeadcbaad691e10d
FelipeDasr <felipe.heiker07@gmail.com>
2022-09-03 19:45:22 -0400

    new util (deleteFile)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,7 +3,7 @@ import util from 'util';
 import fs from 'fs';
 import path from 'path';
 
-export const createFile = async (data: Buffer, fileFile: string) => {
+export const createFile = async (data: Buffer, filePath: string) => {
     const mkdir = util.promisify(fs.mkdir);
     const exists = util.promisify(fs.exists);
     const writeFile = util.promisify(fs.writeFile);
@@ -11,9 +11,14 @@ export const createFile = async (data: Buffer, fileFile: string) => {
     const tempPath = path.resolve('src', 'temp');
 
     if (!await exists(tempPath)) await mkdir(tempPath);
-    await writeFile(fileFile, data, {});
+    await writeFile(filePath, data, {});
 }
 
 export const generateId = () => {
     return nanoid();
+}
+
+export const deleteFile = async (filePath: string) => {
+    const deleteFile = util.promisify(fs.rm);
+    await deleteFile(filePath);
 }
