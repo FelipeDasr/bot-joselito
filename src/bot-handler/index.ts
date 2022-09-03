3b51a1e4391661eb39dfdb429ecbaec52e488414
FelipeDasr <felipe.heiker07@gmail.com>
2022-09-03 11:17:49 -0400

    BotHanfler update

--- src/bot-handler/index.ts
+++ src/bot-handler/index.ts
@@ -1,16 +1,29 @@
-import { Client, Message } from '@open-wa/wa-automate';
+import { Client, Message, decryptMedia } from '@open-wa/wa-automate';
+import { help } from '../functions/help';
+import { makeSticker } from '../functions/makeSticker';
 
 export class BotHandler {
 
     constructor() { }
 
-    private messageTreatment(msg: Message) {
-        console.log(msg);
+    private async messageTreatment(msg: Message, client: Client) {
+        if (!msg.isGroupMsg) return;
+
+        switch (msg.text) {
+            case '>ajuda':
+                return await help(msg, client);
+
+            case '>figurinha':
+                return await makeSticker(msg, client);
+
+            default:
+                return;
+        }
     }
 
     public async start(client: Client) {
-        await client.onAnyMessage(
-            this.messageTreatment
-        );
+        await client.onAnyMessage(async msg => {
+            await this.messageTreatment(msg, client);
+        });
     }
 }
